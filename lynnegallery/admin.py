cb63b783cfac47fc59143b7fdc5cb456bc233c59
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-27 18:19:41 +0300

    ft:registered classes

--- lynnegallery/admin.py
+++ lynnegallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Editor, Pictures,Location,Category
 
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Pictures)
+admin.site.register(Location)
+admin.site.register(Category)
+
+
