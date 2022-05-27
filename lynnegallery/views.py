e25a63d89f730a1de5372c46d09f85f4c48751d2
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-27 18:23:03 +0300

    added functions to search images

--- lynnegallery/views.py
+++ lynnegallery/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Pictures, Category,Location
 
 # Create your views here.
+
+
+def pictures(request):
+    try:
+        pictures = Pictures.objects.all()
+    except Pictures.DoesNotExist:
+        raise Http404()
+    return render(request,"pictures.html", {"pictures":pictures})
+def index(request):
+    all_pictures = Pictures.objects.all()
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    return render(request,'index.html', {'all_pictures':all_pictures,'location_results':location_results,'category_results':category_results})
+
+
+
+def search_results(request):
+
+    if 'picture' in request.GET and request.GET["picture"]:
+        search_term = request.GET.get("picture")
+        searched_pictures = Pictures.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"picture": searched_pictures})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+def get_category(request,category):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Pictures.objects.filter(category = category)
+    return render(request,'index.html',{'all_pictures':category_result,'category_results':category_results,'location_results':location_results})
+
+def get_location(request,location):
+    category_results = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Pictures.objects.filter(location= location)
+    return render(request,'index.html',{'all_pictures':location_result,'category_results':category_results,'location_results':location_results})
