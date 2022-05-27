ae52207e2cd8f7d7d336c4750465d9b5ea62fc5b
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-27 18:19:18 +0300

    added lynnegallery urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path as  url,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('lynnegallery.urls')),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
