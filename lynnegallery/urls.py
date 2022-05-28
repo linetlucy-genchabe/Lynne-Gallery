205c574a7932c9f8739fa5b49f98ce32052afcb9
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-28 15:28:44 +0300

    added path for search page

--- lynnegallery/urls.py
+++ lynnegallery/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     
     url(r'^$',views.index,name='index'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^about/', views.about, name='about'),
     url(r'^pictures/',views.pictures,name ='pictures')
     
 ]
