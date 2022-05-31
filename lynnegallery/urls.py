22a837ea93ef5bfefb09006d761bc1e7cafd66cc
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-31 08:58:57 +0300

    deployment

--- lynnegallery/urls.py
+++ lynnegallery/urls.py
@@ -7,6 +7,8 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^about/', views.about, name='about'),
-    url(r'^pictures/',views.pictures,name ='pictures')
+    url(r'^pictures/',views.pictures,name ='pictures'),
+    url(r'^category/(\w+)', views.get_category,name='get_category'),
+    url(r'^location/(\w+)', views.get_location,name='get_location'),
     
 ]
